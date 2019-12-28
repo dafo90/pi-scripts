ec952ad04e7e2dd115380c722ddd824dc81f136f
dafo90 <41897414+dafo90@users.noreply.github.com>
2019-12-28 21:43:55 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -5,24 +5,23 @@ set -e
 cd "$(dirname "$0")/.."
 
 echo "Installing shutdown button...\n"
-sudo cp shutdown-button.py /usr/local/bin/
+sudo cp shutdown-button/shutdown-button.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/shutdown-button.py
 
 echo "Installing fan manager...\n"
-sudo cp fan-manager.py /usr/local/bin/
+sudo cp fan-manager/fan-manager.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/fan-manager.py
 
 echo "Starting shutdown button...\n"
-sudo cp shutdown-button.sh /etc/init.d/
+sudo cp shutdown-button/shutdown-button.sh /etc/init.d/
 sudo chmod +x /etc/init.d/shutdown-button.sh
 sudo update-rc.d shutdown-button.sh defaults
 sudo /etc/init.d/shutdown-button.sh start
 
 echo "Starting shutdown button...\n"
-sudo cp fan-manager.sh /etc/init.d/
+sudo cp fan-manager/fan-manager.sh /etc/init.d/
 sudo chmod +x /etc/init.d/fan-manager.sh
 sudo update-rc.d fan-manager.sh defaults
 sudo /etc/init.d/fan-manager.sh start
-
 
 echo "All scripts installed."
